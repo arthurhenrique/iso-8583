29c4d9a186ae1d063c635f1e78179be7ce30ee30
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2017-03-19 01:39:00 -0300

    Update pyso8583.py

--- pyso8583.py
+++ pyso8583.py
@@ -1,14 +1,14 @@
 # Get Data Elements on 
-def get_DE(bitmaps):
-    if bitmaps is None:
-        print("[ERROR] Error generate DE - bitmaps null or not exists")
+def get_DE(list_bitmaps):
+    if list_bitmaps is None:
+        print("[ERROR] Error generate DE - list_bitmaps null or not exists")
         return
     data_elements = []
-    for count in range ( 0, len(bitmaps) ):
+    for count in range ( 0, len(list_bitmaps) ):
         length = 64 * ( count + 1 )
         for bit in range ( 1, length + 1 ):
             # is_set identifies which Data Elements is set on bitmap
-            is_set = ( 1 << length - bit ) & bitmaps[count]
+            is_set = ( 1 << length - bit ) & list_bitmaps[count]
             if ( is_set  != 0 ):
                 data_elements.append(bit)
     return data_elements
@@ -20,26 +20,26 @@ def parse_bitmaps(str_bitmap):
     end = 16
     # verifies if is 16 divisible chars
     if ( len(str_bitmap) % 16 == 0 ):
-        div = len(str_bitmap) / 16
+        len_bitmap_max = len(str_bitmap) / 16
     else:
         print("[ERROR] Error in parsing - bitmap not valid")
         return
     # parse and add to a list
-    for count in range( 0, int(div) ):
+    for count in range( 0, int(len_bitmap_max) ):
         list_bitmaps.append(int( str_bitmap[ini:end], 16 ))
         ini = ini + 16
         end = end + 16
     # verifies if which bitmap has `1` on first bit
     for count in range( 0, len(list_bitmaps) ):
-        is_set = ( 1 << 64 - 1) &  list_bitmaps[count]
-        # if the first bit is 0, got error 
+        is_set = ( 1 << 64 - 1 ) &  list_bitmaps[count]
+        # if the first bit is 0, got an error 
         if ( is_set == 0 ):
-            print("[ERROR] Error in set bitmap ", count, " isn't '1'", bin(list_bitmaps[count]),( 1 << 64 - 1) &  list_bitmaps[count] != 0)
+            print("[ERROR] Error in set bitmap ", count, " isn't '1'", bin(list_bitmaps[count]), ( 1 << 64 - 1) &  list_bitmaps[count] != 0)
             return
     return list_bitmaps
 
 # main
 """ ISO 8583 - 3, has 3 bitmaps"""
 bitmap = "800001001C0000408000110000000020800001001C000040"
-parsed = parse_bitmaps(bitmap)
-get_DE(parsed)
+list_bitmaps = parse_bitmaps(bitmap)
+get_DE(list_bitmaps)
