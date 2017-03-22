2cc37552014e4761ca602f9e065a90767b891303
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2017-03-21 22:40:29 -0300

    Update pyso8583.py

--- pyso8583.py
+++ pyso8583.py
@@ -1,45 +1,77 @@
-# Get Data Elements on 
-def get_DE(list_bitmaps):
-    if list_bitmaps is None:
-        print("[ERROR] Error generate DE - list_bitmaps null or not exists")
-        return
-    data_elements = []
-    for count in range ( 0, len(list_bitmaps) ):
-        length = 64 * ( count + 1 )
-        for bit in range ( 1, length + 1 ):
-            # is_set identifies which Data Elements is set on bitmap
-            is_set = ( 1 << length - bit ) & list_bitmaps[count]
-            if ( is_set  != 0 ):
-                data_elements.append(bit)
-    return data_elements
+class Pyso8583:
+    
+    # Property that has DE's actives
+    bitmap = []
 
-# Parse and verify if is a bitmap valid
-def parse_bitmaps(str_bitmap):
-    list_bitmaps = []
-    ini = 0
-    end = 16
-    # verifies if is 16 divisible chars
-    if ( len(str_bitmap) % 16 == 0 ):
-        len_bitmap_max = len(str_bitmap) / 16
-    else:
-        print("[ERROR] Error in parsing - bitmap not valid")
-        return
-    # parse and add to a list
-    for count in range( 0, int(len_bitmap_max) ):
-        list_bitmaps.append(int( str_bitmap[ini:end], 16 ))
-        ini = ini + 16
-        end = end + 16
-    # verifies if which bitmap has `1` on first bit
-    for count in range( 0, len(list_bitmaps) ):
-        is_set = ( 1 << 64 - 1 ) &  list_bitmaps[count]
-        # if the first bit is 0, got an error 
-        if ( is_set == 0 ):
-            print("[ERROR] Error in set bitmap ", count, " isn't '1'", bin(list_bitmaps[count]), ( 1 << 64 - 1) &  list_bitmaps[count] != 0)
+    def __init__(self, bitmap = []):
+        self.bitmap = bitmap
+
+    # Set an string value to property bitmap
+    def set_bitmap(self, str_bitmap):
+        parsed = self.parse_bitmap(str_bitmap)
+        self.bitmap = parsed
+
+    # Returns list[] type 
+    def get_bitmap(self):
+        return self.bitmap
+    
+    # Parse and verify if is a bitmap valid
+    def parse_bitmap(self,str_bitmap):
+        max_bitmaps  = len(str_bitmap) / 16
+        list_bitmaps = []
+        ini = 0
+        end = 16
+        # parse and add to a list
+        for count in range(0, int(max_bitmaps)):
+            list_bitmaps.append(int( str_bitmap[ini:end], 16 ))
+            ini = ini + 16
+            end = end + 16
+        # is bitmap a number or bit valid ?
+        if not (self.is_bitmap_valid(list_bitmaps, str_bitmap)):
+            return 
+        else: 
+            return list_bitmaps
+
+    # Returns Boolean type if bitmap is a bitmap valid
+    def is_bitmap_valid(self, list_bitmaps, str_bitmap):
+        flag_bool = True
+        # Verifies if is 16 divisible chars
+        if ( len(str_bitmap) % 16 != 0 ):
+            print("[ERROR] Error in parsing - bitmap not valid")
+            return False
+        # Verifies to each bitmap if first bit is '1'
+        for count in range(0, len(list_bitmaps)):
+            is_set = ( 1 << 64 - 1 ) &  list_bitmaps[count]
+            # if the first bit is 0, got an error 
+            if ( is_set == 0 ):
+                print("[ERROR] Error in set bitmap ", count, " isn't '1'", bin(list_bitmaps[count]), ( 1 << 64 - 1) &  list_bitmaps[count] != 0)
+                flag_bool = False
+        if flag_bool:
+            return True
+        else:
+            return False
+
+    # Shows the Data Elements Acives in bitmap
+    def get_data_elements(self):
+        data_elements = []
+        list_bitmaps  = self.bitmap
+        # Verify if is Null
+        if list_bitmaps is None:
+            print("[ERROR] Error generate DE - list_bitmaps null or not exists")
             return
-    return list_bitmaps
+        # To each bitmap show the bit setted on
+        for count in range ( 0, len(list_bitmaps) ):
+            length = 64 * ( count + 1 )
+            for bit in range ( 1, length + 1 ):
+                # is_set identifies which Data Elements is set on bitmap
+                is_set = ( 1 << length - bit ) & list_bitmaps[count]
+                if ( is_set  != 0 ):
+                    data_elements.append(bit)
+        return data_elements
 
-# main
-""" ISO 8583 - 3, has 3 bitmaps"""
-bitmap = "800001001C0000408000110000000020800001001C000040"
-list_bitmaps = parse_bitmaps(bitmap)
-get_DE(list_bitmaps)
+
+if __name__ == '__main__':
+    iso = Pyso8583()
+    bitmap = "800010000100000040000000000000004000000000000000"
+    iso.set_bitmap(bitmap)
+    print(iso.get_data_elements())
