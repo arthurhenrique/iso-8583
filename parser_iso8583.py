66d1e9541c770fdf7c3c8259eee4f98b3106a9a5
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2017-04-04 13:25:03 -0300

    Update parser_iso8583.py

--- parser_iso8583.py
+++ parser_iso8583.py
@@ -1,12 +1,20 @@
 from struct_iso8583 import BitmapIso
+from utils import *
 
 class ParserMessageIso:
     # Property `message_iso` is a message type in ISO format ( string ), 
     # that contains 3 basics structs( MTI, Bitmap and Data Elements )
     message_iso = ""
 
+    # Property `mti` contains message type identifier
+    mti = ""
+
     # Property `list_bitmap` List of bitmap
     list_bitmap = []
+
+    # Property `list_bitmap` List of bitmap
+    data_element = ""
+
 
     def __init__(self, message_iso = ""):
         self.message_iso = message_iso
@@ -14,14 +22,14 @@ class ParserMessageIso:
     def set_message_iso(self, message_iso):
         self.message_iso = message_iso
 
-    def get_message_iso(self):
-        return self.message_iso
-
-    def get_parsed_mti(self, ini = 0):
+    def set_mti(self, ini = 0):
         end = 4
-        return self.message_iso[ini:end]
-    
-    def get_parsed_bitmap(self, ini = 4):
+        self.mti = self.message_iso[ini:end]
+
+    def get_mti(self):
+        return self.mti
+
+    def get_bitmap(self, ini = 4):
         end = ini + 16
         bitmap = int(self.message_iso[ini:end], 16)
         self.list_bitmap.append(bitmap)
@@ -32,4 +40,6 @@ class ParserMessageIso:
         return self.list_bitmap
 
     def get_parsed_data_elements(self):
-        return self.message_iso[len(self.list_bitmap):-1]
+        ini = 
+        ini = len(self.list_bitmap) * 16
+        return self.message_iso[ini:-1]
